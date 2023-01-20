8c183750ffbb351b37db0ed53e4f3f11df9b4567
whxxyu <66895144+whxxyu@users.noreply.github.com>
2023-01-20 11:00:30 +0700

    Update trialsocks.sh

--- socks/trialsocks.sh
+++ socks/trialsocks.sh
@@ -16,9 +16,31 @@ sockslink2="socks://$socks_base64@$domain:80?path=/socks5&security=none&host=$do
 sockslink3="socks://$socks_base64@$domain:443?security=tls&encryption=none&type=grpc&serviceName=socks5-grpc&sni=$domain#$user"
 rm -rf /tmp/log
 cat > /var/www/html/socks5/socks5-$user.txt << EOF
-========================
-Format Json Socks5
-========================
+----------------------------------------------------
+           ----- [ Trial Socks5 ] -----
+----------------------------------------------------
+Username      : Socks5-$user
+Password      : $pass
+Domain        : $domain
+Wildcard      : (bug.com).$domain
+ISP           : $ISP
+City          : $CITY
+Port TLS      : 443
+Port NTLS     : 80
+Port gRPC     : 443
+Alt Port TLS  : 2053, 2083, 2087, 2096, 8443
+Alt Port NTLS : 8080, 8880, 2052, 2082, 2086, 2095
+Network       : Websocket, gRPC
+Path          : /socks5
+ServiceName   : socks5-grpc
+Alpn          : h2, http/1.1
+----------------------------------------------------
+Expired On    : $exp
+----------------------------------------------------
+
+----------------------------------------------------
+        ----- [ Format Json Socks5 ] -----
+----------------------------------------------------
 {
 "inbounds": [],
 "outbounds": [
@@ -70,29 +92,31 @@ Format Json Socks5
 }
 }
 }
-===============================
-Link Socks5 Account
-===============================
+----------------------------------------------------
+
+----------------------------------------------------
+            ----- [ Link Socks5 ] -----
+----------------------------------------------------
 Link TLS : socks://$socks_base64@$domain:443?path=/socks5&security=tls&host=$domain&type=ws&sni=$domain#$user
-==============================
+----------------------------------------------------
 Link NTLS : socks://$socks_base64@$domain:80?path=/socks5&security=none&host=$domain&type=ws#$user
-==============================
+----------------------------------------------------
 Link gRPC : socks://$socks_base64@$domain:443?security=tls&encryption=none&type=grpc&serviceName=socks5-grpc&sni=$domain#$user
-==============================
+----------------------------------------------------
 EOF
 ISP=$(cat /usr/local/etc/xray/org)
 CITY=$(cat /usr/local/etc/xray/city)
 systemctl restart xray
 clear
-echo -e "————————————————————————————————————————————————————" | tee -a /user/log-socks5-$user.txt
-echo -e "               Trial Socks5 Account                 " | tee -a /user/log-socks5-$user.txt
-echo -e "————————————————————————————————————————————————————" | tee -a /user/log-socks5-$user.txt
-echo -e "Username      : ${user}" | tee -a /user/log-socks5-$user.txt
-echo -e "Password      : ${pass}" | tee -a /user/log-socks5-$user.txt
-echo -e "Domain        : ${domain}" | tee -a /user/log-socks5-$user.txt
-echo -e "ISP           : ${ISP}" | tee -a /user/log-socks5-$user.txt
-echo -e "City          : ${CITY}" | tee -a /user/log-socks5-$user.txt
-echo -e "Wildcard      : (bug.com).${domain}" | tee -a /user/log-socks5-$user.txt
+echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | tee -a /user/log-socks5-$user.txt
+echo -e "━━━━━ [ Trial Socks5 ] ━━━━━" | tee -a /user/log-socks5-$user.txt
+echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | tee -a /user/log-socks5-$user.txt
+echo -e "Username      : $user" | tee -a /user/log-socks5-$user.txt
+echo -e "Password      : $pass" | tee -a /user/log-socks5-$user.txt
+echo -e "Domain        : $domain" | tee -a /user/log-socks5-$user.txt
+echo -e "Wildcard      : (bug.com).$domain" | tee -a /user/log-socks5-$user.txt
+echo -e "ISP           : $ISP" | tee -a /user/log-socks5-$user.txt
+echo -e "City          : $CITY" | tee -a /user/log-socks5-$user.txt
 echo -e "Port TLS      : 443" | tee -a /user/log-socks5-$user.txt
 echo -e "Port NTLS     : 80" | tee -a /user/log-socks5-$user.txt
 echo -e "Port gRPC     : 443" | tee -a /user/log-socks5-$user.txt
@@ -102,17 +126,17 @@ echo -e "Network       : Websocket, gRPC" | tee -a /user/log-socks5-$user.txt
 echo -e "Path          : /socks5" | tee -a /user/log-socks5-$user.txt
 echo -e "ServiceName   : socks5-grpc" | tee -a /user/log-socks5-$user.txt
 echo -e "Alpn          : h2, http/1.1" | tee -a /user/log-socks5-$user.txt
-echo -e "————————————————————————————————————————————————————" | tee -a /user/log-socks5-$user.txt
-echo -e "Link TLS      : ${sockslink1}" | tee -a /user/log-socks5-$user.txt
-echo -e "————————————————————————————————————————————————————" | tee -a /user/log-socks5-$user.txt
-echo -e "Link NTLS     : ${sockslink2}" | tee -a /user/log-socks5-$user.txt
-echo -e "————————————————————————————————————————————————————" | tee -a /user/log-socks5-$user.txt
-echo -e "Link gRPC     : ${sockslink3}" | tee -a /user/log-socks5-$user.txt
-echo -e "————————————————————————————————————————————————————" | tee -a /user/log-socks5-$user.txt
+echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | tee -a /user/log-socks5-$user.txt
+echo -e "Link TLS      : $sockslink1" | tee -a /user/log-socks5-$user.txt
+echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | tee -a /user/log-socks5-$user.txt
+echo -e "Link NTLS     : $sockslink2" | tee -a /user/log-socks5-$user.txt
+echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | tee -a /user/log-socks5-$user.txt
+echo -e "Link gRPC     : $sockslink3" | tee -a /user/log-socks5-$user.txt
+echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | tee -a /user/log-socks5-$user.txt
 echo -e "Format JSON   : http://$domain:8000/socks5/socks5-$user.txt" | tee -a /user/log-socks5-$user.txt
-echo -e "————————————————————————————————————————————————————" | tee -a /user/log-ss2022-$user.txt
+echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | tee -a /user/log-ss2022-$user.txt
 echo -e "Expired On    : $exp" | tee -a /user/log-socks5-$user.txt
-echo -e "————————————————————————————————————————————————————" | tee -a /user/log-socks5-$user.txt
+echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | tee -a /user/log-socks5-$user.txt
 echo " " | tee -a /user/log-socks5-$user.txt
 echo " " | tee -a /user/log-socks5-$user.txt
 echo " " | tee -a /user/log-socks5-$user.txt
