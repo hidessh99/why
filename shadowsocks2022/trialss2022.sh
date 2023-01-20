46edcd2a720f67d4a9273d9a11e135c505574b41
whxxyu <66895144+whxxyu@users.noreply.github.com>
2023-01-20 10:59:26 +0700

    Update trialss2022.sh

--- shadowsocks2022/trialss2022.sh
+++ shadowsocks2022/trialss2022.sh
@@ -18,84 +18,111 @@ ss2022link2="ss://${ss2022_base64}@$domain:80?path=/shadowsocks2022&security=non
 ss2022link3="ss://${ss2022_base64}@$domain:443?security=tls&encryption=none&type=grpc&serviceName=shadowsocks2022-grpc&sni=$domain#${user}"
 rm -rf /tmp/log
 cat > /var/www/html/shadowsocks2022/shadowsocks2022-$user.txt << END
-===============================
-Shadowsocks 2022 WS (CDN) TLS
-===============================
+----------------------------------------------------
+       ----- [ Trial Shadowsocks 2022 ] -----
+----------------------------------------------------
+Remarks       : SS2022-$user
+Domain        : $domain
+Wildcard      : (bug.com).$domain
+ISP           : $ISP
+City          : $CITY
+Port TLS      : 443
+Port NTLS     : 80
+Port gRPC     : 443
+Alt Port TLS  : 2053, 2083, 2087, 2096, 8443
+Alt Port NTLS : 8080, 8880, 2052, 2082, 2086, 2095
+Cipher        : $cipher2
+Password      : $serverpsk:$userpsk
+Network       : Websocket, gRPC
+Path          : /shadowsocks2022
+ServiceName   : shadowsocks2022-grpc
+Alpn          : h2, http/1.1
+----------------------------------------------------
+Expired On    : $exp
+----------------------------------------------------
+
+----------------------------------------------------
+    ----- [ Shadowsocks 2022 WS (CDN) TLS ] -----
+----------------------------------------------------
 - name: SS2022-$user
-type: ss
-server: $domain
-port: 443
-cipher: $cipher2
-password: $serverpsk:$userpsk
-plugin: v2ray-plugin
-plugin-opts:
-mode: websocket
-tls: true
-skip-cert-verify: true
-host: $domain
-path: "/shadowsocks2022"
-mux: true
-=============================
-Shadowsocks 2022 WS (CDN)
-=============================
+  type: ss
+  server: $domain
+  port: 443
+  cipher: $cipher2
+  password: $serverpsk:$userpsk
+  plugin: v2ray-plugin
+  plugin-opts:
+  mode: websocket
+  tls: true
+  skip-cert-verify: true
+  host: $domain
+  path: "/shadowsocks2022"
+  mux: true
+----------------------------------------------------
+
+----------------------------------------------------
+      ----- [ Shadowsocks 2022 WS (CDN) ] -----
+----------------------------------------------------
 - name: SS2022-$user
-type: ss
-server: $domain
-port: 80
-cipher: $cipher2
-password: $serverpsk:$userpsk
-plugin: v2ray-plugin
-plugin-opts:
-mode: websocket
-tls: false
-skip-cert-verify: false
-host: $domain
-path: "/shadowsocks2022"
-mux: true
-===============================
-Link Shadowsocks 2022 Account
-===============================
+  type: ss
+  server: $domain
+  port: 80
+  cipher: $cipher2
+  password: $serverpsk:$userpsk
+  plugin: v2ray-plugin
+  plugin-opts:
+  mode: websocket
+  tls: false
+  skip-cert-verify: false
+  host: $domain
+  path: "/shadowsocks2022"
+  mux: true
+----------------------------------------------------
+
+----------------------------------------------------
+       ----- [ Link Shadowsocks 2022 ] -----
+----------------------------------------------------
 Link TLS : ss://${ss2022_base64}@$domain:443?path=/shadowsocks2022&security=tls&host=${domain}&type=ws&sni=${domain}#${user}
-==============================
+----------------------------------------------------
 Link NTLS : ss://${ss2022_base64}@$domain:80?path=/shadowsocks2022&security=none&host=${domain}&type=ws#${user}
-==============================
+----------------------------------------------------
 Link gRPC : ss://${ss2022_base64}@$domain:443?security=tls&encryption=none&type=grpc&serviceName=shadowsocks2022-grpc&sni=$domain#${user}
-==============================
+----------------------------------------------------
 END
 ISP=$(cat /usr/local/etc/xray/org)
 CITY=$(cat /usr/local/etc/xray/city)
 systemctl restart xray
 clear
-echo -e "————————————————————————————————————————————————————" | tee -a /user/log-ss2022-$user.txt
-echo -e "           Trial Shadowsocks 2022 Account           " | tee -a /user/log-ss2022-$user.txt
-echo -e "————————————————————————————————————————————————————" | tee -a /user/log-ss2022-$user.txt
-echo -e "Remarks       : ${user}" | tee -a /user/log-ss2022-$user.txt
-echo -e "ISP           : ${ISP}" | tee -a /user/log-ss2022-$user.txt
-echo -e "City          : ${CITY}" | tee -a /user/log-ss2022-$user.txt
-echo -e "Domain        : ${domain}" | tee -a /user/log-ss2022-$user.txt
-echo -e "Wildcard      : (bug.com).${domain}" | tee -a /user/log-ss2022-$user.txt
+echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | tee -a /user/log-ss2022-$user.txt
+echo -e "━━━━━ [ Trial Shadowsocks 2022 ] ━━━━━" | tee -a /user/log-ss2022-$user.txt
+echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | tee -a /user/log-ss2022-$user.txt
+echo -e "Remarks       : $user" | tee -a /user/log-ss2022-$user.txt
+echo -e "Domain        : $domain" | tee -a /user/log-ss2022-$user.txt
+echo -e "Wildcard      : (bug.com).$domain" | tee -a /user/log-ss2022-$user.txt
+echo -e "ISP           : $ISP" | tee -a /user/log-ss2022-$user.txt
+echo -e "City          : $CITY" | tee -a /user/log-ss2022-$user.txt
 echo -e "Port TLS      : 443" | tee -a /user/log-ss2022-$user.txt
 echo -e "Port NTLS     : 80" | tee -a /user/log-ss2022-$user.txt
 echo -e "Port gRPC     : 443" | tee -a /user/log-ss2022-$user.txt
 echo -e "Alt Port TLS  : 2053, 2083, 2087, 2096, 8443" | tee -a /user/log-ss2022-$user.txt
 echo -e "Alt Port NTLS : 8080, 8880, 2052, 2082, 2086, 2095" | tee -a /user/log-ss2022-$user.txt
-echo -e "Cipher        : ${cipher2}" | tee -a /user/log-ss2022-$user.txt
+echo -e "Cipher        : $cipher2" | tee -a /user/log-ss2022-$user.txt
 echo -e "Password      : $serverpsk:$userpsk" | tee -a /user/log-ss2022-$user.txt
 echo -e "Network       : Websocket, gRPC" | tee -a /user/log-ss2022-$user.txt
 echo -e "Path          : /shadowsocks2022" | tee -a /user/log-ss2022-$user.txt
 echo -e "ServiceName   : shadowsocks2022-grpc" | tee -a /user/log-ss2022-$user.txt
 echo -e "Alpn          : h2, http/1.1" | tee -a /user/log-ss2022-$user.txt
-echo -e "————————————————————————————————————————————————————" | tee -a /user/log-ss2022-$user.txt
-echo -e "Link TLS      : ${ss2022link1}" | tee -a /user/log-ss2022-$user.txt
-echo -e "————————————————————————————————————————————————————" | tee -a /user/log-ss2022-$user.txt
-echo -e "Link NTLS     : ${ss2022link2}" | tee -a /user/log-ss2022-$user.txt
-echo -e "————————————————————————————————————————————————————" | tee -a /user/log-ss2022-$user.txt
-echo -e "Link gRPC     : ${ss2022link3}" | tee -a /user/log-ss2022-$user.txt
-echo -e "————————————————————————————————————————————————————" | tee -a /user/log-ss2022-$user.txt
+echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | tee -a /user/log-ss2022-$user.txt
+echo -e "Link TLS      : $ss2022link1" | tee -a /user/log-ss2022-$user.txt
+echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | tee -a /user/log-ss2022-$user.txt
+echo -e "Link NTLS     : $ss2022link2" | tee -a /user/log-ss2022-$user.txt
+echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | tee -a /user/log-ss2022-$user.txt
+echo -e "Link gRPC     : $ss2022link3" | tee -a /user/log-ss2022-$user.txt
+echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | tee -a /user/log-ss2022-$user.txt
 echo -e "Format Clash  : http://$domain:8000/shadowsocks2022/shadowsocks2022-$user.txt" | tee -a /user/log-ss2022-$user.txt
-echo -e "————————————————————————————————————————————————————" | tee -a /user/log-ss2022-$user.txt
+echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | tee -a /user/log-ss2022-$user.txt
 echo -e "Expired On    : $exp" | tee -a /user/log-ss2022-$user.txt
-echo -e "————————————————————————————————————————————————————" | tee -a /user/log-ss2022-$user.txt
+echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | tee -a /user/log-ss2022-$user.txt
 echo " " | tee -a /user/log-ss2022-$user.txt
 echo " " | tee -a /user/log-ss2022-$user.txt
 echo " " | tee -a /user/log-ss2022-$user.txt
