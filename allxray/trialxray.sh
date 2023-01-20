e0312f082a3fa3ed71c1af08edf020ef84faac82
whxxyu <66895144+whxxyu@users.noreply.github.com>
2023-01-20 18:16:55 +0700

    Update trialxray.sh

--- allxray/trialxray.sh
+++ allxray/trialxray.sh
@@ -222,8 +222,9 @@ echo -e "Username Socks5  : $user" | tee -a /user/log-allxray-$user.txt
 echo -e "Password Socks5  : $pass" | tee -a /user/log-allxray-$user.txt
 echo -e "Network          : Websocket, gRPC" | tee -a /user/log-allxray-$user.txt
 echo -e "Alpn             : h2, http/1.1" | tee -a /user/log-allxray-$user.txt
-echo -e "Expired On       : $exp" | tee -a /user/log-allxray-$user.txt
 echo -e "Link Akun        : http://$domain:8000/allxray/allxray-$user.txt" | tee -a /user/log-allxray-$user.txt
+echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | tee -a /user/log-allxray-$user.txt
+echo -e "Expired On       : $exp" | tee -a /user/log-allxray-$user.txt
 echo -e " " | tee -a /user/log-allxray-$user.txt
 echo -e " " | tee -a /user/log-allxray-$user.txt
 echo -e " " | tee -a /user/log-allxray-$user.txt
