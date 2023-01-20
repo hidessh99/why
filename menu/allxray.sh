0c983f17ec65bafe3e2ea39706df5f868486eef1
whxxyu <66895144+whxxyu@users.noreply.github.com>
2023-01-20 11:31:01 +0700

    Update allxray.sh

--- menu/allxray.sh
+++ menu/allxray.sh
@@ -9,7 +9,7 @@ CB='\e[35;1m'
 WB='\e[37;1m'
 clear
 echo -e "${BB}————————————————————————————————————————————————————————${NC}"
-echo -e "             ${WB}----- [ All Xray  Menu ] -----${NC}             " | lolcat -a -d 10
+echo -e "             ${WB}----- [ All Xray  Menu ] -----${NC}             "
 echo -e "${BB}————————————————————————————————————————————————————————${NC}"
 echo -e ""
 echo -e " ${MB}[1]${NC} ${YB}Create Account All Xray${NC} "
