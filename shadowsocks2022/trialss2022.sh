5f040925425e0a61337d02bead7316039e2a8b73
whxxyu <66895144+whxxyu@users.noreply.github.com>
2023-01-22 20:38:33 +0700

    Update trialss2022.sh

--- shadowsocks2022/trialss2022.sh
+++ shadowsocks2022/trialss2022.sh
@@ -26,7 +26,6 @@ ____________________________________________________
 ____________________________________________________
 Remarks       : $user
 Domain        : $domain
-Wildcard      : (bug.com).$domain
 ISP           : $ISP
 City          : $CITY
 Port TLS      : 443
@@ -100,7 +99,6 @@ echo -e "━━━━━ [ Trial Shadowsocks 2022 ] ━━━━━" | tee -a /u
 echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | tee -a /user/log-ss2022-$user.txt
 echo -e "Remarks       : $user" | tee -a /user/log-ss2022-$user.txt
 echo -e "Domain        : $domain" | tee -a /user/log-ss2022-$user.txt
-echo -e "Wildcard      : (bug.com).$domain" | tee -a /user/log-ss2022-$user.txt
 echo -e "ISP           : $ISP" | tee -a /user/log-ss2022-$user.txt
 echo -e "City          : $CITY" | tee -a /user/log-ss2022-$user.txt
 echo -e "Port TLS      : 443" | tee -a /user/log-ss2022-$user.txt
