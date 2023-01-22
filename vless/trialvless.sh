00152d8d929be7c2e4508006106cc976b999970b
whxxyu <66895144+whxxyu@users.noreply.github.com>
2023-01-22 20:43:08 +0700

    Update trialvless.sh

--- vless/trialvless.sh
+++ vless/trialvless.sh
@@ -21,7 +21,6 @@ ____________________________________________________
 ____________________________________________________
 Remarks       : $user
 Domain        : $domain
-Wildcard      : (bug.com).$domain
 ISP           : $ISP
 City          : $CITY
 Port TLS      : 443
@@ -113,7 +112,6 @@ echo -e "━━━━━ [ Trial Xray / Vless ] ━━━━━" | tee -a /user/
 echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | tee -a /user/log-vless-$user.txt
 echo -e "Remarks       : $user" | tee -a /user/log-vless-$user.txt
 echo -e "Domain        : $domain" | tee -a /user/log-vless-$user.txt
-echo -e "Wildcard      : (bug.com).$domain" | tee -a /user/log-vless-$user.txt
 echo -e "ISP           : $ISP" | tee -a /user/log-vless-$user.txt
 echo -e "City          : $CITY" | tee -a /user/log-vless-$user.txt
 echo -e "Port TLS      : 443" | tee -a /user/log-vless-$user.txt
