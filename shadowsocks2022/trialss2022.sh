9240e594ef831e4435b4f225e5f05eb457b0b6cc
whxxyu <66895144+whxxyu@users.noreply.github.com>
2023-01-22 14:56:52 +0700

    Update trialss2022.sh

--- shadowsocks2022/trialss2022.sh
+++ shadowsocks2022/trialss2022.sh
@@ -115,16 +115,11 @@ echo -e "Path          : /shadowsocks2022" | tee -a /user/log-ss2022-$user.txt
 echo -e "ServiceName   : shadowsocks2022-grpc" | tee -a /user/log-ss2022-$user.txt
 echo -e "Alpn          : h2, http/1.1" | tee -a /user/log-ss2022-$user.txt
 echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | tee -a /user/log-ss2022-$user.txt
-echo " " | tee -a /user/log-ss2022-$user.txt
-echo " " | tee -a /user/log-ss2022-$user.txt
-echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | tee -a /user/log-ss2022-$user.txt
 echo -e "Link TLS      : $ss2022link1" | tee -a /user/log-ss2022-$user.txt
 echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | tee -a /user/log-ss2022-$user.txt
 echo -e "Link NTLS     : $ss2022link2" | tee -a /user/log-ss2022-$user.txt
 echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | tee -a /user/log-ss2022-$user.txt
 echo -e "Link gRPC     : $ss2022link3" | tee -a /user/log-ss2022-$user.txt
-echo " " | tee -a /user/log-ss2022-$user.txt
-echo " " | tee -a /user/log-ss2022-$user.txt
 echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | tee -a /user/log-ss2022-$user.txt
 echo -e "Format Clash  : http://$domain:8000/shadowsocks2022/shadowsocks2022-$user.txt" | tee -a /user/log-ss2022-$user.txt
 echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | tee -a /user/log-ss2022-$user.txt
