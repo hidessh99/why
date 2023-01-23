33634efc000bf703d9c6cb15b9c7021c2824a92a
whxxyu <66895144+whxxyu@users.noreply.github.com>
2023-01-23 23:39:48 +0700

    Update add-vmess.sh

--- vmess/add-vmess.sh
+++ vmess/add-vmess.sh
@@ -101,7 +101,7 @@ id            : $uuid
 AlterId       : 0
 Security      : auto
 Network       : Websocket
-Path          : /vmess, /(multipath)
+Path          : /vmess /(multipath)
 ServiceName   : vmess-grpc
 Alpn          : h2, http/1.1
 ____________________________________________________
@@ -112,7 +112,7 @@ ____________________________________________________
 ____________________________________________________
         _____ [ Vmess WS (CDN) TLS ] _____                 
 ____________________________________________________
-- name: Vmess-$user
+- name: vmess-$user
   type: vmess
   server: $domain
   port: 443
@@ -125,7 +125,7 @@ ____________________________________________________
   servername: $domain
   network: ws
   ws-opts:
-    path: /vmess, /(multipath)
+    path: /vmess /(multipath)
     headers:
       Host: $domain
 
@@ -133,7 +133,7 @@ ____________________________________________________
 ____________________________________________________
            _____ [ Vmess WS (CDN) ] _____
 ____________________________________________________
-- name: Vmess-$user
+- name: vmess-$user
   type: vmess
   server: $domain
   port: 80
@@ -146,7 +146,7 @@ ____________________________________________________
   servername: $domain
   network: ws
   ws-opts:
-    path: /vmess, /(multipath)
+    path: /vmess /(multipath)
     headers:
       Host: $domain
 
@@ -154,7 +154,7 @@ ____________________________________________________
 ____________________________________________________
           _____ [ Vmess gRPC (CDN) ] _____
 ____________________________________________________
-- name: Vmess-$user
+- name: vmess-$user
   server: $domain
   port: 443
   type: vmess
