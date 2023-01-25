1349babb4603e198bbac426f6ca886778d8151fb
whxxyu <66895144+whxxyu@users.noreply.github.com>
2023-01-25 22:57:18 +0700

    Update del-xray.sh

--- allxray/del-xray.sh
+++ allxray/del-xray.sh
@@ -11,24 +11,24 @@ clear
 NUMBER_OF_CLIENTS=$(grep -c -E "^#&@ " "/usr/local/etc/xray/config.json")
 if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
 clear
-echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | lolcat -a -d 10
-echo -e "              ${WB}Delete All Xray Account${NC}               "
-echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | lolcat -a -d 10
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
+echo -e "               ${WB}Delete All Xray Account${NC}               "
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
 echo -e "  ${YB}You have no existing clients!${NC}"
-echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | lolcat -a -d 10
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
 read -n 1 -s -r -p "Press any key to back on menu"
 allxray
 fi
 clear
-echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | lolcat -a -d 10
-echo -e "              ${WB}Delete All Xray Account${NC}               "
-echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | lolcat -a -d 10
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
+echo -e "               ${WB}Delete All Xray Account${NC}               "
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
 echo -e " ${YB}User  Expired${NC}  "
-echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | lolcat -a -d 10
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
 grep -E "^#&@ " "/usr/local/etc/xray/config.json" | cut -d ' ' -f 2-3 | column -t | sort | uniq
 echo ""
 echo -e "${YB}tap enter to go back${NC}"
-echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | lolcat -a -d 10
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
 read -rp "Input Username : " user
 if [ -z $user ]; then
 allxray
@@ -39,12 +39,12 @@ rm -rf /var/www/html/allxray/allxray-$user.txt
 rm -rf /user/log-allxray-$user.txt
 systemctl restart xray
 clear
-echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | lolcat -a -d 10
-echo -e "          ${WB}All Xray Account Success Deleted${NC}          "
-echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | lolcat -a -d 10
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
+echo -e "           ${WB}All Xray Account Success Deleted${NC}          "
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
 echo -e " ${YB}Client Name :${NC} $user"
 echo -e " ${YB}Expired On  :${NC} $exp"
-echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | lolcat -a -d 10
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
 echo ""
 read -n 1 -s -r -p "Press any key to back on menu"
 clear
